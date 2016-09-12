12e0b2ff0ea479d999f4cf4d0edc259215d9bc84
Gabriel Walsh <gawalsh@adobe.com>
2016-09-12 14:23:44 -0400

    fix travis

--- app/controllers/essays_controller.rb
+++ app/controllers/essays_controller.rb
@@ -1,9 +1,18 @@
 class EssaysController < InheritedResources::Base
+  def index
+    @essays = Essay.all
+  end
+
+  def show
+    @last_essays = Essay.all
+    @essay = Essay.find(params[:id])
+  end
 
   private
 
-    def essay_params
-      params.require(:essay).permit(:title, :description, :imageable)
-    end
+  def essay_params
+      params.require(:essay).permit(:title, :description, :body, :imageable)
+  end
+
 end
 
