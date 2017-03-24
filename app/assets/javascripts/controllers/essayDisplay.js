b122ef2c7fab13883ced1583b0449007318e5d23
Gabriel Walsh <gawalsh@adobe.com>
2017-03-24 16:43:40 -0400

    Added regex for embedded essay items

--- app/assets/javascripts/controllers/essayDisplay.js
+++ app/assets/javascripts/controllers/essayDisplay.js
@@ -1,5 +1,13 @@
-ImaginaryApplication.controller('essayCtrl', [ '$scope', function($scope) {
+ImaginaryApplication.controller('essayCtrl', [ '$scope', '$sce', function($scope, $sce) {
     $scope.init = function () {
         $scope.essay = gon.essay;
+        $scope.embedItems($scope.essay.body);
+
+    };
+    $scope.embedItems = function(text){
+        var reg = /(\|\|\[)(.*)(]\|\|)/g;
+        var test = text.match(reg);
+        var body = text.replace(reg,'');
+        $scope.essay.html =  $sce.trustAsHtml(body);
     };
 }]);
--- app/assets/javascripts/directives/sharingLink.js
+++ app/assets/javascripts/directives/sharingLink.js
@@ -10,7 +10,7 @@ ImaginaryApplication.directive('sharingLink', ['$window', '$rootScope', function
                         $window.location.href = 'https://www.linkedin.com/shareArticle?mini=true&url='+ $rootScope.sitePath+attr.sharingUrl +'&title='+encodeURIComponent(attr.sharingTitle)+'&summary='+encodeURIComponent(attr.sharingDescription)+'&source='+encodeURIComponent($rootScope.siteTile);
                         break;
                     case 'twitter':
-                        $window.location.href = '"http://twitter.com/share?text='+encodeURIComponent(attr.sharingTitle)+'&url='+$rootScope.sitePath+attr.sharingUrl;
+                        $window.location.href = 'http://twitter.com/share?text='+encodeURIComponent(attr.sharingTitle)+'&url='+$rootScope.sitePath+attr.sharingUrl;
                         break;
                     default:
                         $window.location.href = 'http://www.facebook.com/sharer.php?u='+$rootScope.sitePath+attr.sharingUrl;
