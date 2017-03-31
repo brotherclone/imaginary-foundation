b4744983811dab637ea2525cfc2cf041ea19788c
Gabriel Walsh <gawalsh@adobe.com>
2017-03-31 17:03:27 -0400

    embeds sort of working

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,6 +2,7 @@ var ImaginaryApplication = angular.module('ImaginaryApplication',[])
     .run(function($rootScope){
         $rootScope.sitePath = 'http://www.imaginary.foundation';
         $rootScope.siteName = 'The Imaginary Foundation';
+
     });
 $(document).on('turbolinks:load', function() {
     return angular.bootstrap(document.body, ['ImaginaryApplication']);
--- app/assets/javascripts/controllers/essayDisplay.js
+++ app/assets/javascripts/controllers/essayDisplay.js
@@ -1,13 +1,27 @@
-ImaginaryApplication.controller('essayCtrl', [ '$scope', '$sce', function($scope, $sce) {
+ImaginaryApplication.controller('essayCtrl', [ '$scope', '$sce', '$controller','$http', '$q', function($scope, $sce,$controller,$http,$q) {
+    angular.extend(this, $controller('parsingEmbedsCtrl', {
+        $scope: $scope,
+        $http: $http,
+        $q: $q
+    }));
     $scope.init = function () {
         $scope.essay = gon.essay;
-        $scope.embedItems($scope.essay.body);
-
+        $scope.parseHTML($scope.essay.body);
+        $scope.$watch('processedHTML', function(){
+            var body = $scope.cleanUpCommas($scope.processedHTML);
+            $scope.essay.html = $sce.trustAsHtml(body);
+            $scope.displayEmbeds = true;
+        });
     };
-    $scope.embedItems = function(text){
-        var reg = /(\|\|\[)(.*)(]\|\|)/g;
-        var test = text.match(reg);
-        var body = text.replace(reg,'');
-        $scope.essay.html =  $sce.trustAsHtml(body);
+    // Well this is stupid.
+    $scope.cleanUpCommas = function(txt){
+        txt = txt.replace("<p>,</p>", '');
+        txt = txt.replace(",<pull", '<pull');
+        txt = txt.replace(",<imag", '<imag');
+        txt = txt.replace("quote>,", 'quote>');
+        txt = txt.replace("image>,", 'image>');
+        return txt;
     };
 }]);
+
+
