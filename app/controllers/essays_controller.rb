d1b316484318e3ae15ef6e1dd73dd6d590f80910
Gabriel Walsh <gawalsh@adobe.com>
2017-04-03 14:12:33 -0400

    fixed in controller instead

--- app/controllers/essays_controller.rb
+++ app/controllers/essays_controller.rb
@@ -10,10 +10,29 @@ class EssaysController < InheritedResources::Base
 
   def show
     gon.watch.essay =  @essay
+    @clean = replace_with_directive(@essay)
     respond_to do |format|
       format.html { render :show }
       format.json { render :json => @essay}
     end
+  end
+
+  def replace_with_directive(essay)
+    cleaner_pattern =  Regexp.new(/(\|\|\[)(.*)(\]\|\|)/)
+    matches = essay.body.scan(cleaner_pattern)
+    matches.each do |m|
+      r= m[1].split(',')
+      if r[0] == 'pull-quote'
+        the_quote = PullQuote.find(r[1].to_i)
+        pull_quote_directive  = "<div class='pull-quote'><i class='open'></i>" + the_quote.quote + "<i class='close'></i></div>"
+        essay.body =  essay.body.gsub(/(\|\|\[pull-quote\,#{r[1]}\]\|\|)/,pull_quote_directive)
+      elsif r[0] == 'image'
+        the_image = Image.find(r[1].to_i)
+        img_directive  = "<img src=''" + the_image.image_file + "'>"
+        essay.body =  essay.body.gsub(/(\|\|\[image\,#{r[1]}\]\|\|)/,img_directive)
+      end
+    end
+    essay.body
   end
 
   def edit
