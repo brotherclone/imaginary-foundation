10cac666a123ff812dbf7c30c8e65fe0b1adae03
Gabriel Walsh <gawalsh@adobe.com>
2017-04-07 12:14:10 -0400

    restore good words admin

--- app/admin/goodwords.rb
+++ app/admin/goodwords.rb
@@ -1,12 +1,10 @@
-ActiveAdmin.register Gallery do
-  permit_params :title, :body, :description, :external_url
+ActiveAdmin.register Goodword do
+  permit_params :word, :good, :fromURL
   form(:html => {:multipart => true}) do |f|
     f.inputs do
-      f.input :title
-      f.input :description
-      f.input :body, :input_html => {:class => 'tinymce'}, :label => 'Description'
-      f.input :external_url
-      f.input :image_id, :label => 'Main Image', :as => :select, :collection => Image.all.map{|i| ["#{i.name}", i.id]}
+      f.input :word
+      f.input :good, :as => :radio
+      f.input :fromURL
     end
     f.actions
   end
